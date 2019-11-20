21fa0114a9990f0a96ba7b90dca8c166b133e24c
froggagul <froggagul@gmail.com>
2019-11-21 01:50:27 +0900

    2019 11 21

--- controller/eat.ts
+++ controller/eat.ts
@@ -1,7 +1,17 @@
+import {recipe} from '../model/eat';
 
-export default (date: string, time: string) => {
-    // 월요일~금요일을 18~22로 바꾸기
-    // 조식/중식/석식을 0 ~ 2로 바꾸기
-    // model에서 data 불러오기
-    return "급식";
+export default (date: number, time: number) => {
+    const num:number=18;
+
+    
+
+    if(date>=18&&date<=22){
+    if (time==1) return date+"일의 조식: "+recipe[date-num].조식;
+    else if(time==2) return date+"일의 중식: "+recipe[date-num].중식;
+    else if(time==3) return date+"일의 석식: "+recipe[date-num].석식;
+    else return "time은 0~2중 정수를 입력하세요";
+    }
+    else{
+        return "date는 18~22중 정수를 입력하세요";
+    }
 }
--- routes/index.ts
+++ routes/index.ts
@@ -3,7 +3,7 @@ var router = express.Router();
 import eat from '../controller/eat';
 
 router.get('/', function(req, res, next) {
-  res.send({"to":eat(req.query.date, req.query.time)});
+  res.send(eat(req.query.date, req.query.time));
 });
 
 export default router;
